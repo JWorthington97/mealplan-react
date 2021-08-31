35c246d6b01ea9013db118c443a4c303ae09124b
JWorthington97 <jb.worthington@live.co.uk>
2021-08-31 15:40:06 +0100

    Small comments for plan home

--- src/components/Plan/PlanHome.tsx
+++ src/components/Plan/PlanHome.tsx
@@ -2,7 +2,9 @@ import { Text } from '@chakra-ui/react'
 // import { PlanRecipesContext } from '../../App'
 
 export default function PlanHome(): JSX.Element {
-    // const { planRecipes } = useContext(PlanRecipesContext)
+  // maybe have a ... to assign to days e.g. Monday
+  // need a done button or new plan button at the bottom. Might need a plan_iteration column in the backend
+  // const { planRecipes } = useContext(PlanRecipesContext)
 
     return <Text
           ml={["2vw", "2vw", "2vw", "1vw", 0]}
